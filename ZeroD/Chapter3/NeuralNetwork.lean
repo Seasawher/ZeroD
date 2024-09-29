fb4e1fb6cbca685379c18b55baa4751ac1fad596
Kitamado <47292598+Seasawher@users.noreply.github.com>
2024-09-29 12:03:07 +0000

    進捗 9/29

--- ZeroD/Chapter3/NeuralNetwork.lean
+++ ZeroD/Chapter3/NeuralNetwork.lean
@@ -13,26 +13,78 @@ def stepFunction (x : Float) : Float :=
 #eval sizeOf 5.01
 #eval sizeOf 15.01
 
-/-- numpy の np.arrange のような関数を List への関数として実現したもの -/
-def List.arrange (start stop step : Float) : List Float :=
-  if h : start >= stop || step ≤ 0 then []
-  else
-    have lem : (stop - (start + step)) < (stop - start) := by
-      simp only [ge_iff_le, Bool.or_eq_true, decide_eq_true_eq, not_or, not_le] at h
-      rw [@sub_lt_sub_iff_left]
-      simp_all
-    have : sizeOf (stop - (start + step)) < sizeOf (stop - start) := by
-      sorry
-    start :: List.arrange (start + step) stop step
-termination_by stop - start
+#check Float.floor
 
-#eval! List.arrange (-5.0) 5.0 0.1
+/-- numpy の np.arange のような関数を List への関数として実現したもの
 
--- 停止性を証明するのは難しいかもしれない（それはそれで興味がある）
--- しかし、for 文を使って実装して再帰を避ければきっとうまくいきそう
+停止性の証明を避けるために頑張っている。
+for 文を使うなどしてもいいかも
+-/
+def List.arange (start stop step : Float) : List Float :=
+  let n : Nat := (stop - start) / step |> toNat
+  aux n start
+  where
+    toNat (f : Float) : Nat :=
+      let f' := f.floor
+      if f'.isFinite then f'.toUInt64.toNat else 0
+    aux : Nat → Float → List Float
+      | 0, _ => []
+      | n + 1, v => v :: aux n (v + step)
 
-def SciLean.DataArrayN.arrange [IndexType I] [PlainDataType X]
-    (start stop step : Float) : DataArrayN Float I :=
-  sorry
-  -- if start >= stop then ⊞[]
-  -- else arrange (start) x
+#eval 1.0 / 0.0
+#eval 0.0 / 0.0 |> List.arange.toNat
+#eval List.arange (-5.0) 5.0 0.0
+#eval List.arange (-5.0) 5.0 0.1
+#eval (List.arange (-5.0) 5.0 0.1 |>.head!) = -5.0
+
+/-
+Python の numpy.arange がバグるとされているコード
+
+for i in np.arange(3.0, 4.4, 0.1):
+  print(i)
+-/
+#eval List.arange 3.0 4.4 0.1
+#eval (List.arange 3.0 4.4 0.1).contains 3.0
+#eval (List.arange 3.0 4.4 0.1).contains 4.2
+#eval (List.arange 3.0 4.4 0.1).contains 4.3
+#eval (List.arange 3.0 4.4 0.1).getLast? |>.get!
+#eval ((List.arange 3.0 4.4 0.1).getLast? |>.get!) != 4.4
+
+#check Float.toString
+
+#eval ToString.toString ((List.arange 3.0 4.4 0.1).getLast? |>.get!)
+#check Float
+
+#check List.last'
+
+-- 4.4 が入ってることを検証するコード
+#eval (List.arange 3.0 4.4 0.1) |>.filter (fun x => x > 4.3) |> (fun xs => xs.isEmpty)
+
+#check List.isEmpty_nil
+
+open SciLean DataArrayN DataArray
+
+#check DataArray.push
+#check DataArray.intro
+#check DataArrayN.mk
+
+/-- numpy の np.arange のような関数を DataArray に対して実装したもの -/
+def SciLean.DataArray.arange (start stop step : Float) : DataArray Float := Id.run do
+  let list := List.arange start stop step
+  let mut array : DataArray Float := DataArray.mkEmpty (capacity := list.length)
+  for i in list do
+    array := array.push i
+  return array
+
+#eval DataArray.arange 3.0 4.4 0.1
+#eval DataArray.arange (-5.0) 5.0 0.1
+
+/- start, stop, step から作るのではなくて、
+start, (個数 : Nat), step で作れば IndexType は誤差がなくなる
+-/
+
+-- /-- numpy の np.arange のような関数を DataArrayN に対して実装したもの -/
+-- def SciLean.DataArrayN.arange {I : Type} [IndexType I]
+--     (start stop step : Float) (hk : ((stop - start) / step).floor = size I) : DataArrayN Float I := Id.run do
+--   let mut dataArray := []
+--   sorry
